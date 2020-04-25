90703425dbf8530ab83f30b3fd13e7ac3bffc74f
Don Mudd <58741602+Elton-Don@users.noreply.github.com>
2020-04-25 11:31:25 -0500

    Add files via upload
    Committing the code from QuickDBD design.

---
+++ QuickDBD-Music_artist.sql
@@ -0,0 +1,24 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Vl5hgM
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Billboard" (
+    "id" INTEGER   NOT NULL,
+    "Song" VARCHAR   NOT NULL,
+    "Artist" VARCHAR   NOT NULL,
+    "Weeks_On_#1" INTEGER   NOT NULL,
+    "Weeks_On_Chart" INTEGER   NOT NULL,
+    "Peak_Rank" INTEGER   NOT NULL
+);
+
+CREATE TABLE "Top_20000" (
+    "id" INTEGER   NOT NULL,
+    "artist_lastfm" VARCHAR   NOT NULL,
+    "listeners_lastfm" INTEGER   NOT NULL,
+    "scrobbles_lastfm" INTEGER   NOT NULL
+);
+
+ALTER TABLE "Billboard" ADD CONSTRAINT "fk_Billboard_Artist" FOREIGN KEY("Artist")
+REFERENCES "Top_20000" ("artist_lastfm");
+
